ee45b6a80c32c6cf204e8af7638077368c0b9a47
DA <dima121979@gmail.com>
2023-10-12 23:19:38 +0300

    Emplementing Redux Communication for Submit Note

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -16,8 +16,29 @@ const getNotes = async (ticketId, token) => {
   return response.data;
 };
 
+// Create ticket note
+const createNote = async (noteText, ticketId, token) => {
+  const config = {
+    headers: {
+      // As we remember when we send a "token" its has to be in a headers and in "authorization" filed, So just like we did in the "Postman" we doing the same thing accept we doing at from an app.
+      Authorization: `Bearer ${token}`, // Takes "Bearer" Auth and the "token" which has an user "id".
+    },
+  };
+
+  const response = await axios.post(
+    API_URL + ticketId + '/notes',
+    {
+      text: noteText,
+    },
+    config
+  );
+
+  return response.data;
+};
+
 const noteService = {
   getNotes,
+  createNote,
 };
 
 export default noteService;
--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -31,6 +31,28 @@ export const getNotes = createAsyncThunk(
   }
 );
 
+// Create a ticket note
+export const createNote = createAsyncThunk(
+  'notes/create',
+  async ({ noteText, ticketId }, thunkAPI) => {
+    // console.log(ticketData)
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await noteService.createNote(noteText, ticketId, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message); // Rejected and then put it in the payload
+      // So if sum thing gets wrong the message will passed in ".rejectWithValue(message)"
+    }
+  }
+);
+
 export const noteSlice = createSlice({
   name: 'note',
   initialState,
@@ -48,6 +70,19 @@ export const noteSlice = createSlice({
         state.notes = action.payload;
       })
       .addCase(getNotes.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload; // As we remember if its rejected in "catch(error)" where we have our message and then we call this "rejectWithValue(message)" pass this message in and that send as an "action.payload".
+      })
+      .addCase(createNote.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createNote.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.notes.push(action.payload); // In regular Redux this implementation will not work because this state is inimitable, but because we using Redux tool kit that alow us implement this state in order.
+      })
+      .addCase(createNote.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload; // As we remember if its rejected in "catch(error)" where we have our message and then we call this "rejectWithValue(message)" pass this message in and that send as an "action.payload".
