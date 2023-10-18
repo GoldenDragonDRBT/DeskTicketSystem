f3c4d48dcc336922895fcb43a01fab5dff9f7a21
DA <dima121979@gmail.com>
2023-10-18 09:38:09 +0300

    Correction in "server.js" file

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build'))); // Here setting static folder to the frontend build folder, then we loading the "index.html" file thats in that build folder.
 
-  app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  ); // ".sendFile()" to send "index.html" file that gon be in the build folder of our React project.
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  }); // ".sendFile()" to send "index.html" file that gon be in the build folder of our React project.
 } else {
   app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the Support Desk API' });
